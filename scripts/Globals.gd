ce660e2f649fec881e0cbbbe2e5be4fa84e1011b
Kiran <kiranmeyers@gmail.com>
2025-12-02 09:23:02 -0500

    Refactor dynamic script loading

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -62,3 +62,59 @@ static func v4i_to_5d_coord(v: Vector4i) -> String:
 	var file_char := char("a".unicode_at(0) + file)
 	var rank_num  := rank
 	return "T%d:L%d:%s%d" % [T, L, file_char, rank_num]
+
+##For scripts in a given folder, returns Dictionary(filename, GDScript) for all scripts meeting filter criteria.
+##Filter defaults to Mod scripts.
+func create_script_dict(path: String, filter: Callable = func(m): return m is Mod) -> Dictionary[String, GDScript]:
+	var scenes: Dictionary[String, GDScript] = {}
+
+	var real_path := path
+	# Try the virtual path first (works in the editor if mods/ is in the project)
+	var dir := DirAccess.open(path)
+	if dir == null:
+		var fs_path = OS.get_executable_path().get_base_dir().path_join(path.get_file())
+		dir = DirAccess.open(fs_path)
+		real_path = fs_path
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+
+	while file_name != "":
+		if file_name == "." or file_name == "..":
+			file_name = dir.get_next()
+			continue
+		var full_path := real_path.path_join(file_name)
+		if dir.current_is_dir():
+			printlog("[MOD]   Skipped directory: %s" % file_name)
+		else:
+			# Accept .gd, .gd.remap, .gdc, .gde
+			var is_script_like := (
+				file_name.ends_with(".gd")
+				or file_name.ends_with(".gd.remap")
+				or file_name.ends_with(".gdc")
+				or file_name.ends_with(".gde") # Godot 4 debug bytecode
+			)
+			if not is_script_like:
+				printlog("[MOD]   Skipped (not script-like file): %s" % file_name)
+			else:
+				var script_path := full_path
+				if script_path.ends_with(".gd.remap"):
+					script_path = script_path.get_basename()  # drop .remap
+
+				var script = load(script_path)
+				if not (script is GDScript):
+					printlog("[MOD]   Skipped (not GDScript). Type:", typeof(script))
+				else:
+					var inst = script.new()
+					if filter.call(inst):
+						var mod_name := file_name.get_basename().get_basename()
+						scenes[mod_name] = script
+					else:
+						printlog("[MOD]   Skipped (instance is not Mod), class:", inst.get_class())
+		file_name = dir.get_next()
+	dir.list_dir_end()
+	printlog("[MOD] Finished. Total mods found:", scenes.size())
+	return scenes
+
+static func printlog(...args):
+	pass
--- scripts/main.gd
+++ scripts/main.gd
@@ -6,14 +6,13 @@ static var chess := preload("res://scenes/chess.tscn")
 const mod_path := "res://mods"
 const game_modes_path := "res://scripts/game_modes"
 
-var mod_dict : Dictionary[String, String]
-var game_mode_dict : Dictionary[String, String]
-
+var mod_dict : Dictionary[String, GDScript]
+var game_mode_dict : Dictionary[String, GDScript]
 
 func _ready():
 	run_tests()
-	mod_dict = create_mod_dict(mod_path)
-	game_mode_dict = create_mod_dict(game_modes_path)
+	mod_dict = Globals.create_script_dict(mod_path)
+	game_mode_dict = Globals.create_script_dict(game_modes_path)
 	
 	var mods : BoxContainer = $MainMenu/Options/Mods
 	for child in mods.get_children():
@@ -24,7 +23,6 @@ func _ready():
 		var checkbox = CheckBox.new()
 		checkbox.text = mod
 		mods.add_child(checkbox)
-		mod_dict = create_mod_dict(mod_path)
 
 	var dropdown: OptionButton = $MainMenu/Options/Modes/Options
 	dropdown.clear()
@@ -38,14 +36,12 @@ func _on_start_pressed() -> void:
 	var selected_mods: Array[Mod] = []
 	for child in $MainMenu/Options/Mods.get_children():
 		if child is CheckBox and child.button_pressed:
-			var scene_path: String = mod_dict.get(child.text)
-			var ps := load(scene_path)
-			selected_mods.append(ps.new())
+			var script: GDScript = mod_dict.get(child.text)
+			selected_mods.append(script.new())
 	
 	var mode_dropdown = $MainMenu/Options/Modes/Options
-	var mode_path = game_mode_dict.get(mode_dropdown.get_item_text(mode_dropdown.selected))
-	selected_mods.append(load(mode_path).new())
-	
+	var mode_script: GDScript = game_mode_dict.get(mode_dropdown.get_item_text(mode_dropdown.selected))
+	selected_mods.append(mode_script.new())
 	instance.load_mods(selected_mods)
 	change_scene(instance)
 
@@ -54,83 +50,7 @@ func change_scene(scene: Node):
 		child.queue_free()
 	add_child(scene)
 
-func create_mod_dict(path: String) -> Dictionary[String, String]:
-	var scenes: Dictionary[String, String] = {}
-
-	# Try the virtual path first (works in the editor if mods/ is in the project)
-	var dir := DirAccess.open(path)
-	var real_path := path
-
-	# If that fails (exported game), fall back to a folder next to the .exe
-	if dir == null and path.begins_with("res://"):
-		var folder_name := path.get_file()        # "mods" from "res://mods"
-		var exe_dir := OS.get_executable_path().get_base_dir()
-		var fs_path := exe_dir.path_join(folder_name)
-
-		dir = DirAccess.open(fs_path)
-		if dir == null:
-			push_error("[MOD] Cannot open folder: %s or %s" % [path, fs_path])
-			return scenes
-		else:
-			printlog("[MOD] Using external mods folder:", fs_path)
-			real_path = fs_path
-	elif dir == null:
-		push_error("[MOD] Cannot open folder: %s" % path)
-		return scenes
-
-	dir.list_dir_begin()
-	var file_name := dir.get_next()
-
-	while file_name != "":
-		if file_name == "." or file_name == "..":
-			file_name = dir.get_next()
-			continue
-
-		var full_path := real_path.path_join(file_name)
-
-		if dir.current_is_dir():
-			printlog("[MOD]   Skipped directory: %s" % file_name)
-		else:
-			# Accept .gd, .gd.remap, .gdc, .gde
-			var is_script_like := (
-				file_name.ends_with(".gd")
-				or file_name.ends_with(".gd.remap")
-				or file_name.ends_with(".gdc")
-				or file_name.ends_with(".gde") # Godot 4 debug bytecode
-			)
-
-			if not is_script_like:
-				printlog("[MOD]   Skipped (not script-like file): %s" % file_name)
-			else:
-				var script_path := full_path
-				if script_path.ends_with(".gd.remap"):
-					script_path = script_path.get_basename()  # drop .remap
-
-				var script = load(script_path)
-				if script == null:
-					push_error("[MOD]   ERROR: failed to load script: %s" % script_path)
-				elif not (script is GDScript):
-					printlog("[MOD]   Skipped (not GDScript). Type:", typeof(script))
-				else:
-					var inst = script.new()
-					if inst is Mod:
-						var mod_name := file_name.get_basename().get_basename()
-						scenes[mod_name] = script_path
-						printlog("[MOD]   ADDED mod:", mod_name, "=>", script_path)
-					else:
-						printlog("[MOD]   Skipped (instance is not Mod), class:", inst.get_class())
-
-		file_name = dir.get_next()
-
-	dir.list_dir_end()
-
-	printlog("[MOD] Finished. Total mods found:", scenes.size())
-	return scenes
-
 func run_tests():
 	var test_node := preload(TEST_SCRIPT).new()
 	self.add_child(test_node)
 	test_node.run_tests()
-
-func printlog(...args):
-	pass
