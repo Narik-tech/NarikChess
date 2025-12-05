49f9d9f2493ed119f2324e4edb7958dbdded6f3c
Kiran <kiranmeyers@gmail.com>
2025-12-05 18:38:40 -0500

    tweak camera panning

--- scripts/user_interface/camera_pan.gd
+++ scripts/user_interface/camera_pan.gd
@@ -1,28 +1,31 @@
 ## Summary: Camera controller that pans and zooms based on input actions.
 extends Camera2D
 
-const speed = 10
+const SPEED = 1000
 const scrollSpeed = .1
+@export var scroll_speed := 0.3
+@export var min_zoom := Vector2.ONE * 0.1
+@export var max_zoom := Vector2.ONE * 6.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity := Vector2.ZERO
-	if Input.is_action_pressed("Down") : velocity += Vector2.DOWN * speed
-	if Input.is_action_pressed("Left") : velocity += Vector2.LEFT * speed
-	if Input.is_action_pressed("Right") : velocity += Vector2.RIGHT * speed
-	if Input.is_action_pressed("Up") : velocity += Vector2.UP * speed
-	position += velocity
-	
-	if Input.is_action_pressed("Scroll Out") : 
-		print("zoomind")
-		zoom += Vector2.ONE * scrollSpeed
-	if Input.is_action_pressed("Scroll In") : zoom -= Vector2.ONE * scrollSpeed
+
+	if Input.is_action_pressed("Down"):
+		velocity += Vector2.DOWN * SPEED
+	if Input.is_action_pressed("Left"):
+		velocity += Vector2.LEFT * SPEED
+	if Input.is_action_pressed("Right"):
+		velocity += Vector2.RIGHT * SPEED
+	if Input.is_action_pressed("Up"):
+		velocity += Vector2.UP * SPEED
+
+	if velocity != Vector2.ZERO:
+		position += velocity * (1/zoom.x) * delta
 		
-	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Scroll Out"):
-		zoom -= Vector2.ONE * scrollSpeed
-	if Input.is_action_pressed("Scroll In"): 
-		zoom += Vector2.ONE * scrollSpeed
-	if zoom < Vector2.ONE * .1:
-		zoom = Vector2.ONE * .1
+		zoom -= Vector2.ONE * scroll_speed
+	elif event.is_action_pressed("Scroll In"):
+		zoom += Vector2.ONE * scroll_speed
+
+	zoom = zoom.clamp(min_zoom, max_zoom)
