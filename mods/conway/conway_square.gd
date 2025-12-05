eaaae395682aa42ac3eacc01af9826d48ef073e1
Kiran <kiranmeyers@gmail.com>
2025-12-05 18:15:56 -0500

    update textures

--- mods/conway/conway_square.gd
+++ mods/conway/conway_square.gd
@@ -1,9 +1,10 @@
 ## Piece that blocks movement and represents a Conway life cell.
 class_name ConwaySquare
 extends Piece
+@export var conway_texture: Texture2D
 
 static func inst() -> ConwaySquare:
-	return Piece.color_inst(Color.FOREST_GREEN, ConwaySquare)
+	return Piece.texture_inst(load("res://mods/conway/conway_piece.png"), ConwaySquare)
 	
 func blocks_movement(_piece_moving: ChessPiece) -> bool:
 	return true
